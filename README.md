2fa6eb88aab7c30299fcdd18df85450b4c087bc9
Caleb Connolly <caleb.connolly@linaro.org>
2025-04-11 15:07:30 +0200

    Initial commit
    Signed-off-by: Caleb Connolly <caleb.connolly@linaro.org>

---
+++ README.md
@@ -0,0 +1,70 @@
+# mkcab
+
+Build .cab archives for U-Boot releases for Qualcomm boards.
+
+## Install
+
+Simplest is to just do:
+
+```sh
+pipx install --editable .
+```
+
+Then you can hack on the script and not worry about anything.
+
+## Usage
+
+```txt
+usage: mkcab [-h] uboot guid codename model version
+
+Create a CAB file for U-Boot firmware with dynamic GUIDs.
+
+positional arguments:
+  uboot       Path to U-Boot binary
+  guid        GUID for the firmware
+  codename    Board codename (should match base compatible with the vendor prefix removed)
+  model       Model name
+  version     Release version
+
+options:
+  -h, --help  show this help message and exit
+```
+
+Compile U-Boot following the board specific instructions, and pack it
+into whatever format is needed (e.g. signed MBN or Android boot image).
+
+Determine the capsule UUID used for your board with:
+
+```sh
+$ .output/tools/mkeficapsule guidgen .output/dts/upstream/src/arm64/qcom/qcs6490-rb3gen2.dtb UBOOT_UEFI_PARTITION
+Generating GUIDs for qcom,qcs6490-rb3gen2 with namespace 8c9f137e-91dc-427b-b2d6-b420faebaf2a:
+UBOOT_UEFI_PARTITION: FDA31DED-6E9C-5C44-BE37-B46994BEB543
+```
+
+Update the path to be the DTB used by your board, the last parameter is
+the fw_image identifier, it must be one of:
+
+* `UBOOT_UEFI_PARTITION` - for rb3g2 and other boards which have U-Boot
+  on the "uefi" partition
+* `UBOOT_XBL_PARTITION` - for boards with U-Boot embedded into the XBL
+  elf
+* `UBOOT_BOOT_PARTITION` - for boards that chainload U-Boot via the
+  Android bootloader
+
+Then build the capsule using `mkeficapsule` again, for the rb3gen2 this
+is:
+
+```sh
+$ .output/tools/mkeficapsule -g FDA31DED-6E9C-5C44-BE37-B46994BEB543 -i 1 u-boot.mbn rb3g2-uboot.capsule
+```
+
+Use the appropriate input file for your board (be it mbn, android boot
+image, whatever) and the GUID you generated.
+
+Finally, we can embed this all into a LVFS compatible .cab file with:
+
+```sh
+$ mkcab .output/u-boot.mbn FDA31DED-6E9C-5C44-BE37-B46994BEB543 qcs6490-rb3gen2 "RB3 Gen 2" 2025.04-1
+Created u-boot-qcs6490-rb3gen2.cab
+```
+
---
+++ mkcab.py
@@ -0,0 +1,82 @@
+
+import jinja2
+import cabarchive
+import sys
+import argparse
+import time
+from pathlib import Path
+
+metainfo_template = """
+<?xml version="1.0" encoding="UTF-8"?>
+<!-- Copyright 2024 Linaro Ltd.
+     Author: Caleb Connolly <caleb.connolly@linaro.org> -->
+<component type="firmware">
+  <id>org.linaro.u-boot.qcom.{{ codename }}.firmware</id>
+  <name>U-Boot {{ codename }}</name>
+  <name_variant_suffix>U-Boot for {{ model }}</name_variant_suffix>
+  <summary>Qualcomm {{ model }} U-Boot</summary>
+  <description>
+    <p>
+    </p>
+  </description>
+  <provides>
+    <firmware type="flashed">{{ guid }}</firmware>
+  </provides>
+  <url type="homepage">https://git.codelinaro.org/linaro/qcomlt/u-boot</url>
+  <metadata_license>CC0-1.0</metadata_license>
+  <project_license>GPL-2.0</project_license>
+  <releases>
+    <release urgency="medium" version="{{ version }}" date="{{ date }}" install_duration="120">
+      <checksum filename="firmware.bin" target="content"/>
+    </release>
+  </releases>
+  <custom>
+    <value key="LVFS::VersionFormat">number</value>
+    <value key="LVFS::UpdateProtocol">org.uefi.capsule</value>
+  </custom>
+  <keywords>
+    <keyword>uefi</keyword>
+    <keyword>u-boot</keyword>
+    <keyword>bootloader</keyword>
+  </keywords>
+</component>
+"""
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="Create a CAB file for U-Boot firmware with dynamic GUIDs.")
+    parser.add_argument("uboot", type=str, help="Path to U-Boot binary")
+    parser.add_argument("guid", type=str, help="GUID for the firmware")
+    parser.add_argument("codename", type=str, help="Board codename (should match base compatible with the vendor prefix removed)")
+    parser.add_argument("model", type=str, help="Model name")
+    parser.add_argument("version", type=str, help="Release version")
+    return parser.parse_args()
+
+def main():
+    args = parse_args()
+    guid = args.guid.lower()
+    if len(guid) != 36:
+        print(f"Invalid GUID: {guid}. Must be 36 characters long.")
+        sys.exit(1)
+    if not Path(args.uboot).is_file():
+        print(f"U-Boot binary not found: {args.uboot}")
+        sys.exit(1)
+
+    # Create the metainfo.xml file
+    template = jinja2.Template(metainfo_template)
+    metainfo = template.render(
+        codename=args.codename,
+        model=args.model,
+        guid=guid,
+        version=args.version,
+        date=time.strftime("%Y-%m-%dT%H:%M:%S", time.gmtime())
+    )
+    # print(metainfo)
+
+    arc = cabarchive.CabArchive()
+    arc[f"firmware.bin"] = cabarchive.CabFile(open(args.uboot, "rb").read())
+    arc[f"firmware.metainfo.xml"] = cabarchive.CabFile(metainfo.encode())
+
+    with open(f"u-boot-{args.codename}.cab", "wb") as f:
+        f.write(arc.save())
+
+    print(f"Created u-boot-{args.codename}.cab")
